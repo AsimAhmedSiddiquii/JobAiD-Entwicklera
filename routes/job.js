2a5695de79ce00ad3949a86a72d15197cd352036
maseeh-k <80687369+maseeh-k@users.noreply.github.com>
2023-02-02 19:51:53 +0530

    Added

--- routes/job.js
+++ routes/job.js
@@ -1,19 +1,19 @@
 const router = require("express").Router();
-const mongoose = require("mongoose");
+const checkLogin = require('../middlewares/login');
 
 const Resume = require("../models/resume");
 const Job = require("../models/job");
 
-router.get("/", async (req, res, next) => {
+router.get("/", checkLogin, async (req, res, next) => {
   const resume = await Resume.findOne({ userId: req.session.userid }).exec();
   console.log(resume)
-  if(resume.skills) {
+  if(resume && resume.skills) {
     const jobs = await Job.find({
       skills: { $in: resume.skills },
     }).exec();
     res.render('jobs');
   } else {
-    res.redirect('/resume')
+    res.redirect('/resume/education')
   }
 });
 
