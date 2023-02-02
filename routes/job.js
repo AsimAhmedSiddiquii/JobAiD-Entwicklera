e6ab99d088a060685d5b40cce66e8fd60af9397c
maseeh-k <80687369+maseeh-k@users.noreply.github.com>
2023-02-02 23:34:51 +0530

    Added Resume functionality

--- routes/job.js
+++ routes/job.js
@@ -6,8 +6,7 @@ const Job = require("../models/job");
 
 router.get("/", checkLogin, async (req, res, next) => {
   const resume = await Resume.findOne({ userId: req.session.userid }).exec();
-  console.log(resume)
-  if(resume && resume.skills) {
+  if (resume && resume.skills) {
     const jobs = await Job.find({
       skills: { $in: resume.skills },
     }).exec();
