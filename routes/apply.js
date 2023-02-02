b26ededf5c693c70ed354f5a69af10784f28f2ab
rayyan-mulla <rayyan.mulla@somaiya.edu>
2023-02-03 01:57:27 +0530

    Designed Applied Job UI

--- routes/apply.js
+++ routes/apply.js
@@ -43,63 +43,63 @@ router.get('/', checkLogin, async (req,res,next)=>{
   
   <table>
     <tr>
-      <th colspan='2' >Personal Details</th>
+      <th colspan='2' style="text-align: center">Personal Details</th>
       
     </tr>
     <tr>
       <th>Name</th>
-      <td>` + userData.name + `</td>
+      <td>` + userData.userId.name + `</td>
     </tr>
     <tr>
       <th>Email</th>
-      <td>` + userData.email + `</td>
+      <td>` + userData.userId.email + `</td>
     </tr>
     
     <tr>
       <th>Address</th>
-      <td>` + userData.address + `</td>
+      <td>` + userData.userId.address + `</td>
     </tr>
     <tr>
       <th>Phone</th>
-      <td>` + userData.phone + `</td>
+      <td>` + userData.userId.phone + `</td>
     </tr>
     
     <tr>
-      <th colspan='2' >Educational Details</th>
+      <th colspan='2' style="text-align: center">Educational Details</th>
       
     </tr>
     <tr>
       <th>Institute Name</th>
-      <td>` + userData.institute_name + `</td>
+      <td>` + userData.education_experience[0].institute_name + `</td>
     </tr>
     <tr>
       <th>Degree Title</th>
-      <td>` + userData.degree_title + `</td>
+      <td>` + userData.education_experience[0].degree_title + `</td>
     </tr>
       
     <tr>
-      <th colspan='2' >Work Experience</th>
+      <th colspan='2' style="text-align: center">Work Experience</th>
       
     </tr>
     <tr>
       <th>Company Name</th>
-      <td>` + userData.company_name + `</td>
+      <td>` + userData.work_experience[0].company_name + `</td>
     </tr>
     <tr>
       <th>Role</th>
-      <td>` + userData.role + `</td>
+      <td>` + userData.work_experience[0].role + `</td>
     </tr>
     
     <tr>
       <th>Year of Experience</th>
-      <td>` + userData.yoe + `</td>
+      <td>` + userData.work_experience[0].yoe + `</td>
     </tr>
     
     <tr>
-      <th colspan='2' >Skills</th>
+      <th colspan='2' style="text-align: center">Skills</th>
     </tr>
     <tr>
-      <td>` + userData.skills + `</td>
+      <td colspan='2' >` + userData.skills + `</td>
     </tr>
     
   </table>
@@ -123,5 +123,9 @@ router.get('/', checkLogin, async (req,res,next)=>{
     }
     });
 })
+
+router.get("/applied", (req, res, next) => {
+  res.render('job/applied');
+});
 
 module.exports = router;
