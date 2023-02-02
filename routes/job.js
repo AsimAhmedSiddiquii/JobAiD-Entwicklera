3432a96fb517622587c0a7b754fe6eccd5638917
maseeh-k <80687369+maseeh-k@users.noreply.github.com>
2023-02-02 19:18:21 +0530

    Added Dashboard Voice

--- routes/job.js
+++ routes/job.js
@@ -3,16 +3,18 @@ const router = require("express").Router();
 const Resume = require("../models/resume");
 const Job = require("../models/job");
 
-router.get("/", (req, res, next) => {
-  res.render('jobs')
-});
-
-router.get("/:userid", async (req, res, next) => {
-  const resume = await Resume.findOne({ userId: req.params.userid }).exec();
-  const jobs = await Job.find({
-    skills: { $in: resume.skills },
-  }).exec();
-  res.json(jobs);
+router.get("/", async (req, res, next) => {
+  const resume = await Resume.findOne({ userId: req.session.userid }).exec();
+  console.log(resume)
+  if(resume.skills) {
+    const jobs = await Job.find({
+      skills: { $in: resume.skills },
+    }).exec();
+    res.render('jobs');
+  } else {
+    res.redirect('/resume')
+  }
+  
 });
 
 module.exports = router;
