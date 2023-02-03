0213c252979133a1db756a2e5b495ce9d03972d8
rayyan-mulla <rayyan.mulla@somaiya.edu>
2023-02-03 07:27:32 +0530

    Added Voice to Job Preferences

--- routes/job.js
+++ routes/job.js
@@ -20,16 +20,4 @@ router.post("/preferences", async (req, res, next) => {
   res.render("job/results", { jobs });
 });
 
-// router.get("/results", checkLogin, async (req, res, next) => {
-//   const resume = await Resume.findOne({ userId: req.session.userid }).exec();
-//   if (resume && resume.skills) {
-//     const jobs = await Job.find({
-//       skills: { $in: resume.skills },
-//     }).exec();
-//     res.render("job/results");
-//   } else {
-//     res.redirect("/resume/education");
-//   }
-// });
-
 module.exports = router;
